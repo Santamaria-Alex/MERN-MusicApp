e34b38a27bea5ecca8b61803e9353c8237d5b978
Alex Santamaria <santamariaalex146@gmail.com>
2022-03-21 16:07:39 -0500

    starting frontend, added routes to main

--- frontend/music_crud/src/App.js
+++ frontend/music_crud/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Albums from "./pages/Albums";
+import Artist from "./pages/Artist";
+import Search from "./pages/Search";
+import Songs from "./pages/Songs";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/artist" element={<Artist />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/songs" element={<Songs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
